af4153805b2d8dfdf12a7186b5264c60112f33be
Ireneusz Kuliga <irekkuliga@gmail.com>
2016-07-19 15:13:54 +0200

    add achievements

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-var AppID = require('./lib/steam/appid');
-
+const AppID = new require('./lib/steam/appid');
+const Achievements = require('./lib/steam/achievements');
 function SteamAppInfo() {}
 
-SteamAppInfo.prototype.AppID = new AppID();
+SteamAppInfo.prototype.AppID = AppID;
+SteamAppInfo.prototype.Achievements = Achievements;
+
+SteamAppInfo.prototype.Achievements.getByName = function(name,lang) {
+  return new Promise(function(resolve, reject) {
+    new AppID().first(name).then(id => new Achievements().get(id, lang).then( data => resolve(data)).catch(err => reject(err))).catch( err => reject(err));
+  });
+};
 
 module.exports = new SteamAppInfo();
